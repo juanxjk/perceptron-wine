f195d8520b66b0d525fc219e56a748f2d03ee9f5
Juan Carlos <juanksoliveira@gmail.com>
2017-05-17 02:30:59 -0300

    First commit

---
+++ src/Perceptron.java
@@ -0,0 +1,13 @@
+import java.util.List;
+
+public class Perceptron {
+	private List<Wine> getWineData() {
+		return null;
+		// TODO
+	}
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ src/Wine.java
@@ -0,0 +1,126 @@
+
+public class Wine {
+	private double alcohol;
+	private double malicAcid;
+	private double ash;
+	private double alcalinityOfAsh;
+	private double magnesium;
+	private double totalPhenols;
+	private double flavanoids;
+	private double nonflavanoidPhenols;
+	private double proanthocyanins;
+	private double colorIntensity;
+	private double hue;
+	private double oD280;
+	private double Proline;
+
+	public Wine(double alcohol, double malicAcid, double ash, double alcalinityOfAsh, double magnesium,
+			double totalPhenols, double flavanoids, double nonflavanoidPhenols, double proanthocyanins,
+			double colorIntensity, double hue, double oD280, double Proline) {
+	}
+
+	public double getAlcohol() {
+		return alcohol;
+	}
+
+	public void setAlcohol(double alcohol) {
+		this.alcohol = alcohol;
+	}
+
+	public double getMalicAcid() {
+		return malicAcid;
+	}
+
+	public void setMalicAcid(double malicAcid) {
+		this.malicAcid = malicAcid;
+	}
+
+	public double getAsh() {
+		return ash;
+	}
+
+	public void setAsh(double ash) {
+		this.ash = ash;
+	}
+
+	public double getAlcalinityOfAsh() {
+		return alcalinityOfAsh;
+	}
+
+	public void setAlcalinityOfAsh(double alcalinityOfAsh) {
+		this.alcalinityOfAsh = alcalinityOfAsh;
+	}
+
+	public double getMagnesium() {
+		return magnesium;
+	}
+
+	public void setMagnesium(double magnesium) {
+		this.magnesium = magnesium;
+	}
+
+	public double getTotalPhenols() {
+		return totalPhenols;
+	}
+
+	public void setTotalPhenols(double totalPhenols) {
+		this.totalPhenols = totalPhenols;
+	}
+
+	public double getFlavanoids() {
+		return flavanoids;
+	}
+
+	public void setFlavanoids(double flavanoids) {
+		this.flavanoids = flavanoids;
+	}
+
+	public double getNonflavanoidPhenols() {
+		return nonflavanoidPhenols;
+	}
+
+	public void setNonflavanoidPhenols(double nonflavanoidPhenols) {
+		this.nonflavanoidPhenols = nonflavanoidPhenols;
+	}
+
+	public double getProanthocyanins() {
+		return proanthocyanins;
+	}
+
+	public void setProanthocyanins(double proanthocyanins) {
+		this.proanthocyanins = proanthocyanins;
+	}
+
+	public double getColorIntensity() {
+		return colorIntensity;
+	}
+
+	public void setColorIntensity(double colorIntensity) {
+		this.colorIntensity = colorIntensity;
+	}
+
+	public double getHue() {
+		return hue;
+	}
+
+	public void setHue(double hue) {
+		this.hue = hue;
+	}
+
+	public double getoD280() {
+		return oD280;
+	}
+
+	public void setoD280(double oD280) {
+		this.oD280 = oD280;
+	}
+
+	public double getProline() {
+		return Proline;
+	}
+
+	public void setProline(double proline) {
+		Proline = proline;
+	}
+
+}
